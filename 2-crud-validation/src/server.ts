997b225e7d5f43738d4e6ab229c2cf0f8cc1d836
Serhii Holovko <serhii.holovko@kingfisher.com>
2020-08-09 16:40:42 +0300

    Implement homework-2

---
+++ 2-crud-validation/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import userRouter from "./routes/user";
+import dotenv from "dotenv";
+import "./logger";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use("/", userRouter);
+
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.listen(port, () => {
+  console.log(`listening for requests on port ${host}:${port}`);
+});
